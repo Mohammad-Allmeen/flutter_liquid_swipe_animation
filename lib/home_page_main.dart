a1d784ba6d72bf0713fabbba2db92980c0caef3c
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2024-10-31 08:28:10 +0530

    Initial Commit

--- lib/home_page_main.dart
+++ lib/home_page_main.dart
@@ -1,15 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePageMain extends StatefulWidget {
+  const HomePageMain({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePageMain> {
   final LiquidController liquidController = LiquidController();
 
   final pages = [
@@ -61,21 +62,56 @@ class _HomePageState extends State<HomePage> {
             const Center(child: Icon(Icons.error)),
       ),
     ),
+    SizedBox(
+      height: double.infinity,
+      child: CachedNetworkImage(imageUrl:
+      'https://images.pexels.com/photos/11118835/pexels-photo-11118835.jpeg?auto=compress&cs=tinysrgb&w=1200',
+      fit: BoxFit.cover,
+        placeholder: (context, url)=> const Center(child: CircularProgressIndicator(),),
+        errorWidget: (context, url,error) => const Center(child: Icon(Icons.error),),
+      ),
+    )
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LiquidSwipe(
-        pages: pages,
-        waveType: WaveType.liquidReveal,
-        slideIconWidget: const Icon(Icons.arrow_back_ios, color: Colors.white),
-        positionSlideIcon: 0.8,
-        onPageChangeCallback: (page) {
-          print(page);
-        },
-        liquidController: liquidController,
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          LiquidSwipe(
+            pages: pages,
+            waveType: WaveType.liquidReveal,
+            //enableSideReveal: true,
+            slideIconWidget: const Icon(Icons.arrow_back_ios, color: Colors.white),
+            positionSlideIcon: 0.9,
+            onPageChangeCallback: (page) {
+              print(page);
+              setState(() {
+
+              });
+            },
+            liquidController: liquidController,
+          ),
+          Positioned(
+            bottom: 20,
+            child: AnimatedSmoothIndicator(
+              activeIndex: liquidController.currentPage,
+              count: pages.length,
+              effect: ExpandingDotsEffect(
+                dotHeight: 10,
+                dotWidth: 10,
+                activeDotColor: Colors.white,
+                dotColor: Colors.grey.shade400,
+              ),
+              onDotClicked: (index) {
+                liquidController.animateToPage(page: index);
+              },
+            ),
+          ),
+        ],
       ),
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:liquid_swipe_example/one.dart';
+import 'package:liquid_swipe_example/home_page_main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const HomePageMain(),
     );
   }
 }
